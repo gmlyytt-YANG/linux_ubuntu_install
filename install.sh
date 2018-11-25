f78bc81ecc51b7b988b7dcebc37f469b54d817ed
gmlyytt-YANG <gmlyytt@outlook.com>
2018-11-25 19:45:55 +0800

    add install

--- install.sh
+++ install.sh
@@ -6,25 +6,39 @@ if [ "$1" = "install" ]; then
     wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
     sudo apt-get update
     sudo apt-get install google-chrome-stable
+    /usr/bin/google-chrome-stable
 
     # necessary softwares
-    sudo apt-get install vim git cmake 
-  
-    # chinese input 
-    wget http://cdn2.ime.sogou.com/dl/index/1524572264/sogoupinyin_2.2.0.0108_amd64.deb?st=eN7IKw-whY5bYeUQ5xLlxg&e=1543115719&fn=sogoupinyin_2.2.0.0108_amd64.deb
+    sudo apt-get install vim git cmake zsh 
 
+    # zsh 
+    chsh -s /bin/zsh
+    echo "when failed to chsh: PAM认证 -> sudo vim /etc/passwd, not change first line"
+    # oh my zsh
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  
     # pip 
     # mkdir -p ~/.pip
     # vim ~/.pip/pip/conf
     # add the content:
     # [global]
     # index-url = https://pypi.tuna.tsinghua.edu.cn/simple/
-    
+   
+    # pycharm
+    wget https://download.jetbrains.8686c.com/python/pycharm-professional-2018.3.tar.gz
+    echo "tar -zxvf of it, find the shell in bin" 
+
+    # clion 
+    wget https://download.jetbrains.8686c.com/cpp/CLion-2018.2.6.tar.gz
+    echo "tar -zxvf of it, find the shell in bin" 
+
     # anaconda 
     wget https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
     bash Anaconda3-5.3.1-Linux-x86_64.sh
     sudo echo export PATH=$PATH:~/anaconda3/bin >> ~/.bashrc
+    sudo echo export PATH=$PATH:~/anaconda3/bin >> ~/.zshrc
     source ~/.bashrc
+    source ~/.zshrc
     conda create -n yl python=3.5
     
     # python
