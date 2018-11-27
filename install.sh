511ec499975022840f56c2c953e1fc76ef04785b
gmlyytt-YANG <gmlyytt@outlook.com>
2018-11-27 11:15:50 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -9,7 +9,16 @@ if [ "$1" = "install" ]; then
     /usr/bin/google-chrome-stable
 
     # necessary softwares
-    sudo apt-get install vim git cmake zsh 
+    sudo apt-get install vim git cmake zsh curl
+    sudo echo "set nu" >> ~/.vimrc
+    sudo echo "set expandtab" >> ~/.vimrc
+    sudo echo "set ts=4" >> ~/.vimrc
+    sudo echo "set autoindent" >> ~/.vimrc
+
+    # terminal theme
+    git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+    bash gome-terminal-colors-solarized/set_dark.sh
+    echo "input 1 YES 2"
 
     # zsh 
     chsh -s /bin/zsh
@@ -17,36 +26,30 @@ if [ "$1" = "install" ]; then
     # oh my zsh
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   
-    # pip 
-    # mkdir -p ~/.pip
-    # vim ~/.pip/pip/conf
-    # add the content:
-    # [global]
-    # index-url = https://pypi.tuna.tsinghua.edu.cn/simple/
-   
     # pycharm
     wget https://download.jetbrains.8686c.com/python/pycharm-professional-2018.3.tar.gz
-    echo "tar -zxvf of it, find the shell in bin" 
-
-    # clion 
-    wget https://download.jetbrains.8686c.com/cpp/CLion-2018.2.6.tar.gz
-    echo "tar -zxvf of it, find the shell in bin" 
+    tar -xzvf pycharm-professional-2018.3.tar.gz
+    sh pycharm-2018.3/pycharm.sh
 
     # anaconda 
     wget https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
     bash Anaconda3-5.3.1-Linux-x86_64.sh
     sudo echo export PATH=$PATH:~/anaconda3/bin >> ~/.bashrc
     sudo echo export PATH=$PATH:~/anaconda3/bin >> ~/.zshrc
-    source ~/.bashrc
-    source ~/.zshrc
+    conda config --add channels 'https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/'
+    conda config --set show_channel_urls yes
     conda create -n yl python=3.5
     
     # python
     source activate yl
     conda install tensorflow=1.3.0 keras matplotlib scikit-learn scikit-image pandas 
     pip install opencv-python
+    
+    # clion 
+    wget https://download.jetbrains.8686c.com/cpp/CLion-2018.2.6.tar.gz
+    echo "tar -zxvf of it, find the shell in bin" 
+else
+    # change xrandr
+    xrandr --newmode "1920x1080_60.00" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
+    xrandr --addmode DVI-I-1 "1920x1080_60.00"
 fi
-
-# change xrandr
-xrandr --newmode "1920x1080_60.00" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
-xrandr --addmode DVI-I-1 "1920x1080_60.00"
